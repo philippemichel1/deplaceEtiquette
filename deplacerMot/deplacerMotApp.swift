379e824ea7bfe4dcf882b1eb7cc541ee293d1c02
Philippe MICHEL <philippe.michel1@gmail.com>
2021-03-02 14:27:24 +0100

    Initial Commit

---
+++ deplacerMot/deplacerMotApp.swift
@@ -0,0 +1,17 @@
+//
+//  deplacerMotApp.swift
+//  deplacerMot
+//
+//  Created by Philippe MICHEL on 02/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct deplacerMotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
